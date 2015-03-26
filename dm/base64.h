70481e6af6db2ffce0e165824f357b46aa2c9f06
Bromium Developer <opensource-developer@bromium.com>
2015-03-26 16:59:37 -0700

    uXen - a type-2 hypervisor
    further history may be made available later
    
    To include once available:
    - git fetch <git uxen remote> refs/replace/*:refs/replace/*

---
+++ dm/base64.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2012-2015, Bromium, Inc.
+ * Author: Julian Pidancet <julian@pidancet.net>
+ * SPDX-License-Identifier: ISC
+ */
+
+#ifndef _BASE64_H_
+#define _BASE64_H_
+
+unsigned char *base64_decode(const char *input, size_t *output_len);
+char *base64_encode(const unsigned char *data, size_t len);
+
+#endif /* _BASE64_H_ */
