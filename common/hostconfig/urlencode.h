8a435d0aeab21b49fa65669eb1468d14c1b14d18
Julian Pidancet <julian@pidancet.net>
2015-05-28 11:39:30 +0100

    hostconfig: urlencode ACPI OEM strings as they may contain characters unparseable in JSON

---
+++ common/hostconfig/urlencode.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2015, Bromium, Inc.
+ * Author: Julian Pidancet <julian@pidancet.net>
+ * SPDX-License-Identifier: ISC
+ */
+
+#ifndef _URLENCODE_H_
+#define _URLENCODE_H_
+
+char *urlencode(const unsigned char *data, size_t len);
+
+#endif /* _URLENCODE_H_ */
