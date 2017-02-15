214f2922dc815afadfd69a482799721fe3954b6d
Tomasz Wroblewski <tomasz.wroblewski@bromium.com>
2017-02-15 10:41:40 +0100

    uxendm/uxenkmdod/uxenevent: when application starts using userspace framebuffer, toggle any existing real fb mapping in dwm process into scratch mappings. And vice versa when disabling userspace fb.

--- dm/guest-agent.h
+++ dm/guest-agent.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2016, Bromium, Inc.
+ * Copyright 2015-2017, Bromium, Inc.
  * SPDX-License-Identifier: ISC
  */
 
@@ -17,6 +17,7 @@ int guest_agent_mouse_event(uint32_t x, uint32_t y, int32_t dv, int32_t dh,
 int guest_agent_window_event(uint64_t hwnd, uint64_t message,
                              uint64_t wParam, uint64_t lParam);
 int guest_agent_set_dynamic_time_zone(void *dtzi);
+int guest_agent_user_draw_enable(int enable);
 
 int guest_agent_cleanup(void);
 int guest_agent_init(void);
